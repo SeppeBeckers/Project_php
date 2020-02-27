7e02b78fb04ba68a12d8de727da34ed196fa5d8c
SeppeBeckers <r0749133@student.thomasmore.be>
2020-02-27 13:57:44 +0100

    login werkt en localisatie naar nederlands

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -16,12 +18,24 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('admin')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
+
+        // Insert admin
+        DB::table('users')->insert(
+            [
+                [
+                    'name' => 'admin',
+                    'password' => Hash::make('admin1234'),
+                    'admin' => true,
+                    'created_at' => now()
+                ]
+
+            ]
+        );
     }
 
     /**
--- resources/views/shared/navigation.blade.php
+++ resources/views/shared/navigation.blade.php
@@ -1,46 +1,62 @@
 
 <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-    <div class="container">
+                <div class="container">
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        Hotel Kempenrust
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
 
-        <a class="navbar-brand" href="/"><h1>Hotel Kempenrust</h1></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsNav">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="collapsNav">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="/">Home</a>
-                </li>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <!-- Left Side Of Navbar -->
+                        <ul class="navbar-nav mr-auto">
 
-                <li class="nav-item">
-                    <a class="nav-link" href="/contact-us">Contact</a>
-                </li>
-            </ul>
-            {{--  Auth navigation  --}}
-            <ul class="navbar-nav ml-auto">
-                @guest
-                    <li class="nav-item">
-                        <a class="nav-link" href="../admin/overview.blade.php"><i class="fas fa-sign-in-alt"></i>Admin Login</a>
-                    </li>
-                @endguest
-                @auth
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#!" data-toggle="dropdown">
-                            {{ auth()->user()->name }} <span class="caret"></span>
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-right">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="/">Home</a>
+                                </li>
 
-                            @if(auth()->user()->admin)
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="/admin/genres"><i class="fas fa-microphone-alt"></i>Overview</a>
-                                <a class="dropdown-item" href="/admin/records"><i class="fas fa-compact-disc"></i>Kamers beheren</a>
-                                <a class="dropdown-item" href="/admin/records"><i class="fas fa-compact-disc"></i>Reserveringen</a>
-                                <a class="dropdown-item" href="/admin/records"><i class="fas fa-compact-disc"></i>rekening</a>
-                            @endif
-                        </div>
-                    </li>
-                @endauth
-            </ul>
-        </div>
-    </div>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="/contact-us">Contact</a>
+                                </li>
+
+                        </ul>
+
+                        <!-- Right Side Of Navbar -->
+                        <ul class="navbar-nav ml-auto">
+                            <!-- Authentication Links -->
+                            @guest
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                </li>
+                                @if (Route::has('register'))
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    </li>
+                                @endif
+                            @else
+                                <li class="nav-item dropdown">
+                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                        {{ Auth::user()->name }} <span class="caret"></span>
+                                    </a>
+
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="../admin/overview"><i class="fas fa-microphone-alt"></i>Overview</a>
+                                        <a class="dropdown-item" href="../admin/room/overview"><i class="fas fa-compact-disc"></i>Kamers beheren</a>
+                                        <a class="dropdown-item" href="../admin/reservation"><i class="fas fa-compact-disc"></i>Reserveringen</a>
+                                        <a class="dropdown-item" href="../admin/bill/consult"><i class="fas fa-compact-disc"></i>rekening</a>
+                                        <a class="dropdown-item" href="{{ route('logout') }}"
+                                           onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                            {{ __('Logout') }}
+                                        </a>
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                            @csrf
+                                        </form>
+                                    </div>
+                                </li>
+                            @endguest
+                        </ul>
+                    </div>
+                </div>
 </nav>
