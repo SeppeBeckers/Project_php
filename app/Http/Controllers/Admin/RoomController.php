c93ccae51e6a203b95d2ffbcb566815cf51d439c
SeppeBeckers <r0749133@student.thomasmore.be>
2020-03-12 14:36:57 +0100

    Overview kamers beheren

--- app/Http/Controllers/Admin/RoomController.php
+++ app/Http/Controllers/Admin/RoomController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\NotAvailable;
 use App\Room;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -11,9 +12,10 @@ class RoomController extends Controller
 {
     public function index()
     {
-        $rooms = Room::orderBy('room_number')
-            ->get();
-        $result = compact('rooms');
+        $not_availables = NotAvailable::orderBy('room_id')->get();
+        $rooms = Room::orderBy('room_number')->get();
+
+        $result = compact('rooms', 'not_availables');
         Json::dump($result);
         return view('admin.room.overview', $result);
     }
--- routes/web.php
+++ routes/web.php
@@ -24,14 +24,14 @@ Route::view('/', 'start');
 
 Route::get('reservation/book', 'ReservationController@index');
 Route::get('reservation/data', 'ReservationController@store');
-Route::get('admin/room', 'Admin\RoomController@index');
+
 
 
 Route::middleware(['auth', 'admin'])->prefix('admin')->group(function () {
     route::redirect('/', 'overview');
     Route::get('overview', 'Admin\OverviewController@index');
     Route::resource('reservation', 'Admin\ReservationController');
-    //Route::resource('room', 'Admin\RoomController@index');
+    Route::resource('room', 'Admin\RoomController');
     Route::resource('arrangement', 'Admin\ArrangementController');
     Route::resource('bill', 'Admin\BillController');
 });
