1df166785eb3b27ecf5b93071a8202068300ebe3
BrentVervecken <r0740410@student.thomasmore.be>
2020-03-12 12:50:07 +0100

    basis overview

--- resources/views/admin/overview.blade.php
+++ resources/views/admin/overview.blade.php
@@ -4,10 +4,10 @@
 
 
 @section('main')
-    <h1>Genres</h1>
+    <h1>Overzicht boekingen</h1>
     <p>
-        <a href="/admin/genres/create" class="btn btn-outline-success">
-            <i class="fas fa-plus-circle mr-1"></i>Create new genre
+        <a href="../reservation/book" class="btn btn-outline-success">
+            <i class="fas fa-plus-circle mr-1"></i>Maak een nieuwe boeking
         </a>
     </p>
     <div class="table-responsive">
@@ -18,29 +18,36 @@
                 <th>Start datum</th>
                 <th>Eind datum</th>
                 <th>Naam</th>
+                <th>Voorschot</th>
             </tr>
             </thead>
             <tbody>
-            @foreach($room_reservations as $reservation)
+            @foreach($Reservation as $reservation)
                 <tr>
                     <td>{{ $reservation->room_id }}</td>
                     <td>{{ $reservation->starting_date }}</td>
                     <td>{{ $reservation->end_date }}</td>
-                    <td>{{ $reservation->name }}</td>
+                    <td>{{ $reservation->Reservation->name }}</td>
+                    @if($reservation->Reservation->with_deposit == 1)
+                        <td><i class="fas fa-check"></i></td>
+                    @else
+                        <td></td>
+                    @endif
+
                     <td>
                         <form action="" method="post">
                             @method('delete')
                             @csrf
                             <div class="btn-group btn-group-sm">
-                                <a href="" class="btn btn-outline-success"
+                                <a href="reservation" class="btn btn-outline-success"
                                    data-toggle="tooltip"
-                                   title="Edit">
-                                    <i class="fas fa-edit"></i>
+                                   title="Edit {{$reservation->Reservation->name}}">
+                                    <i class="fas fa-edit">Edit</i>
                                 </a>
                                 <button type="submit" class="btn btn-outline-danger"
                                         data-toggle="tooltip"
-                                        title="Delete">
-                                    <i class="fas fa-trash-alt"></i>
+                                        title="Delete {{$reservation->Reservation->name}}">
+                                    <i class="fas fa-trash-alt">Verwijder</i>
                                 </button>
                             </div>
                         </form>
