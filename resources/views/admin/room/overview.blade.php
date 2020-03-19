a08e032cfc9f483128ba22a806f0bc20f516a6cb
SeppeBeckers <r0749133@student.thomasmore.be>
2020-03-19 15:10:39 +0100

    beschikbare kamers Deel 1

--- resources/views/admin/room/overview.blade.php
+++ resources/views/admin/room/overview.blade.php
@@ -2,6 +2,9 @@
 @section('title', 'Overzicht kamers')
 
 @section('main')
+    <style>
+
+    </style>
     <h1>Overzicht kamers</h1>
     <div class="table-responsive">
         <table class="table">
@@ -10,9 +13,8 @@
                 <th>Foto</th>
                 <th>Kamernr</th>
                 <th>Beschrijving</th>
-                <th>Beschikbaar</th>
-                <th>Van</th>
-                <th>Tot</th>
+                <th>Beschikbaar?</th>
+                <th></th>
             </tr>
             </thead>
             <tbody>
@@ -26,26 +28,43 @@
      @else
 {{$room->maximum_persons}} personen
  @endif</td>
-                    @foreach($not_availables as $not_available)
-                    @if ($not_available->room_id === $room-> id)
-                            <td></td>
-                        <td>{{$not_available->starting_date}}</td>
-                        <td>{{$not_available->end_date}}</td>
-                        @else
-                            <td><i class="fas fa-check">check</i></td>
-                            <td></td>
-                            <td></td>
-                        @endif
-                    @endforeach
+                    <td>
+                        @foreach($not_availables as $not_available)
+                            @if ($not_available->room_id === $room->room_number)
+                                @if ($standard_date >  $not_available->starting_date && $standard_date < $not_available->end_date || $not_available == null)
+                                    <i class="fas fa-times"></i>
+                                @else
+                                    <i class="fas fa-check"></i>
+                                @endif
+                            @endif
+                        @endforeach
+                            @if ($not_available->room_id != $room->room_number)
+                                <i class="fas fa-check"></i>
+                                @endif
+
+                    </td>
+                   <td>
+                       <form action="/admin/room/{{ $room->id }}" method="post">
+                           @method('edit')
+                           @csrf
+                           <div class="btn-group btn-group-sm">
+                               <a href="/admin/room/{{ $room->id }}/not_available" class="btn btn-outline-success"
+                                  data-toggle="tooltip"
+                                  title="Bekijk onbeschikbaarheden van kamer {{ $room->room_number }}">
+                                   <i class="fas fa-edit"> Onbeschikbaarheden</i>
+                               </a>
+                           </div>
+                       </form>
+                   </td>
                     <td>
                         <form action="/admin/room/{{ $room->id }}" method="post">
-                            @method('delete')
+                            @method('edit')
                             @csrf
                             <div class="btn-group btn-group-sm">
                                 <a href="/admin/room/{{ $room->id }}/edit" class="btn btn-outline-success"
                                    data-toggle="tooltip"
                                    title="Bewerk kamer {{ $room->room_number }}">
-                                    <i class="fas fa-edit">Bewerken</i>
+                                    <i class="fas fa-edit"> Bewerken</i>
                                 </a>
                             </div>
                         </form>
