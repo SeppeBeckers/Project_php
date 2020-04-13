fbb836a49cc4463bb0e579fec671a56284c6804a
SeppeBeckers <r0749133@student.thomasmore.be>
2020-04-13 15:48:02 +0200

    CRUD werkt niet bij onbeschikbaarheden en heb routes terug toegevoegd

---
+++ resources/views/admin/room/modal.blade.php
@@ -0,0 +1,39 @@
+{{-- Model voor data te bewerken --}}
+<div class="modal" id="modal-not">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">modal-genre-title</h5>
+                <button type="button" class="close" data-dismiss="modal">
+                    <span>&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form action="" method="post">
+                    @method('')
+                    @csrf
+                    <div class="form-group">
+                        <label for="starting_date">Van: </label>
+                        <input type="date" name="starting_date" id="starting_date"
+                               class="form-control"
+                               placeholder="Starting_date"
+                               minlength=" "
+                               required
+                               value="">
+                        <div class="invalid-feedback"></div>
+                        <br>
+                        <label for="end_date">Tot: </label>
+                        <input type="date" name="end_date" id="end_date"
+                               class="form-control"
+                               placeholder="end_date"
+                               minlength=" "
+                               required
+                               value="">
+                        <div class="invalid-feedback"></div>
+                    </div>
+                    <button type="submit" class="btn btn-success">Bewaar de datums</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
