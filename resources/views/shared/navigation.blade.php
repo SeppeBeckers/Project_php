c5e28fb0e7e23fa64b5be21972011bdcd3c4645f
SeppeBeckers <r0749133@student.thomasmore.be>
2020-04-23 15:43:56 +0200

    kleine fix

--- resources/views/shared/navigation.blade.php
+++ resources/views/shared/navigation.blade.php
@@ -43,7 +43,7 @@
 
                 @endguest
                 <li class="nav-item ml-md-5">
-                    <a class="nav-link handleiding" href="/assets/Handleiding.pfd"> <i class="fas fa-question-circle mr-1"></i>Handleiding</a>
+                    <a class="nav-link handleiding" href="/assets/Handleiding.pdf"> <i class="fas fa-question-circle mr-1"></i>Handleiding</a>
                 </li>
             </ul>
 
